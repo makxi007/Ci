26cc5a09ab4f7fdbf2f5ff9e281ec93b2dcc253b
Maks <Mtolstov_2000@mail.ru>
2020-09-21 21:54:08 +0600

    Commit 5

--- ciphers/vigenere_cipher.c
+++ ciphers/vigenere_cipher.c
@@ -9,36 +9,31 @@ void print_array(char array[], int length){
 	{
 		printf("%c", array[i]);
 	}
-}	
-
-bool Alpha(char sentence[], int len){
-	char check_result[5];
-	for (int i = 0; i < len; ++i)
-	{
-		if (isalpha(sentence[i]))
-		{
-			
-			return true;
-		}
-		else{
-		
-			return false;
-		}
-	}
-
-	
 }
 
-
 int main(void){
-	char sentence[] = "meet me in the park at eleven am";
-	int sentence_length = 32;
-	char sentence2[sentence_length];
-	char sentence3[sentence_length];
-	char key[] = "bacon";
-	int key_legnth =  strlen(key);
-	int num_ci;
-	int count = 0;
+	char sentence[] = "cubik Hello";
+	int len_sentence = strlen(sentence);
+	char sentence2[len_sentence];
+	char key[] = "zero";
+	int len_key = strlen(key);
+	int ci;
+	
+	for (int i = 0; i < len_sentence; ++i)
+	{
+		if (isalpha(sentence[i])){
+			
+			sentence2[i] = key[i%len_key];
+ 		}
+ 		else{
+ 			sentence2[i] = 32;
+ 		}
+ 		//ci = (sentence[i] + key[i] - 65 ) % 26 + 65;		
+	}
+
+	print_array(sentence2, len_sentence);
+	printf("\n");
+
 
 	/* Print key in alhabetic order 
 	char temp;
@@ -57,14 +52,5 @@ int main(void){
 	print_array(key, 5);
 	*/
 
-	for (int a = 0; a < sentence_length; ++a)
-	{
-		char x = (sentence[a] + key[a])%26;
-
-		x += 65;
-
-		sentence2[a] = (char)(x);
-	}
-	print_array(sentence2, sentence_length);
-	printf("\n");
+	
 }
